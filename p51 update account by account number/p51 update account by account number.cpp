3fee811ee96dddcf09486d96d096c6371ca39d07
gomaa <gogochemist35@gmail.com>
2023-10-11 16:22:55 +0300

    change code

--- p51 update account by account number/p51 update account by account number.cpp
+++ p51 update account by account number/p51 update account by account number.cpp
@@ -13,23 +13,20 @@ struct stClient
 	string name;
 	string phone;
 	double balance;
+	bool markToDelete = false;
+	bool markToUpdate = false;
 };
-
-stClient readClient()
+stClient readClient(string AccountNumber)
 {
 	stClient client;
 
-	cout << "Please Enter Client Data?\n\n";
+	client.Number = AccountNumber;
 
-	cout << "Enter Account Number? ";
-	getline(cin >> ws, client.Number);
+	cout << "\nEnter PinCode? ";
+	cin >> client.pinCode;
 
 	cout << "Enter Name? ";
-	getline(cin, client.name);
-
-
-	cout << "Enter PinCode? ";
-	cin >> client.pinCode;
+	getline(cin >> ws, client.name);
 
 	cout << "Enter Phone ";
 	cin >> client.phone;
@@ -39,7 +36,6 @@ stClient readClient()
 
 	return client;
 }
-
 string readAccountNumber()
 {
 	string AccountNumber = "";
@@ -48,17 +44,27 @@ string readAccountNumber()
 
 	return AccountNumber;
 }
-
 char readAnswerToDeleteAccount()
 {
-	char confirmUpdate;
+	char confirmDelete;
 
-	cout << "Are You sure to update This Account? y/n? ";
-	cin >> confirmUpdate;
+	cout << "\nAre You sure to delete This Account? y/n ";
+	cin >> confirmDelete;
 
-	return confirmUpdate;
+	return confirmDelete;
 
 }
+char readAnswerToUpdateAccount()
+{
+	char confirmDelete;
+
+	cout << "\nAre You sure to update This Account? y/n ";
+	cin >> confirmDelete;
+
+	return confirmDelete;
+
+}
+
 
 vector <string> LineDataToVector(string line)
 {//A150#//#1234#//#Gomaa ElSherbini#//#00925365232#//#5000.000000
@@ -97,26 +103,26 @@ stClient VectorDataToStClient(vector <string> vClient)
 	return client;
 }
 
-vector <string> vectorStringtAccounts(string fileName)
+vector <stClient> loadDataToVector(string fileName)
 {
-	fstream Clients;
+	fstream ClientsFile;
 	string line;
-	vector <string> vStringClients;
+	vector <stClient> vStClients;
 
-	Clients.open(fileName, ios::in);
+	ClientsFile.open(fileName, ios::in);
 
-	if (Clients.is_open())
+	if (ClientsFile.is_open())
 	{
-		while (getline(Clients, line))
+		while (getline(ClientsFile, line))
 		{
 			if (line != "")
 			{
-				vStringClients.push_back(line);
+				vStClients.push_back(VectorDataToStClient(LineDataToVector(line)));
 			}
 		}
-		Clients.close();
+		ClientsFile.close();
 	}
-	return vStringClients;
+	return vStClients;
 }
 
 string convertStAccountToStringLine(stClient clientRecord)
@@ -131,100 +137,337 @@ string convertStAccountToStringLine(stClient clientRecord)
 	recordAataLine += delim + to_string(clientRecord.balance);
 
 	return recordAataLine;
-
 }
 
 void printStClientData(stClient client)
 {
 	cout << "\nThe following are the eclient details:\n\n";
 
-	cout << " Account Number:  " << client.Number << endl;
-	cout << " Pin Code:  " << client.pinCode << endl;
-	cout << " Name:  " << client.name << endl;
-	cout << " Phone: " << client.phone << endl;
-	cout << " Balance: " << client.balance << endl;
-
+	cout << " Account Number       :  " << client.Number << endl;
+	cout << " Pin Code             :  " << client.pinCode << endl;
+	cout << " Name                 :  " << client.name << endl;
+	cout << " Phone                : " << client.phone << endl;
+	cout << " Balance              : " << client.balance << endl;
 }
 
-bool findAccountWithAccountNumber(vector <string> vStringClients, string AccountNumber, stClient& client)
+bool findAccountWithAccountNumber(vector <stClient>& vStClients, string AccountNumber, stClient& targtedClient)
 {
-	vector <string> vClient;
-	stClient sClient;;
-
-	for (string line : vStringClients)
+	for (stClient& client : vStClients)
 	{
-		sClient = VectorDataToStClient(LineDataToVector(line));
-		if (sClient.Number == AccountNumber)
+		if (client.Number == AccountNumber)
 		{
-			client = sClient;
+			//client.markToDelete = true;
+			client.markToUpdate = true;
+			targtedClient = client;
 			return 1;
 		}
 	}
 	return 0;
 }
 
-void updateAccountWithAccountNumber(string fileName, vector <string> vFileContent, string lineToUpdate, string lineToUpdateTo)
+stClient updateClient(vector <stClient>&vStClients, stClient clientToUpdate, string AccountNumber)
+{
+	stClient updatedClient = readClient(AccountNumber);
+
+	for (stClient &client : vStClients)
+	{
+		if (client.markToUpdate == clientToUpdate.markToUpdate)
+		{
+			client = updatedClient;
+			break;
+		}
+
+	}
+	return clientToUpdate = updatedClient;
+}
+
+void saveAccountsToFile(string fileName, vector <stClient> vStClients)
 {
 	fstream myFile;
+
 	myFile.open(fileName, ios::out);
 
 	if (myFile.is_open())
 	{
-		for (string& line : vFileContent)
+		for (stClient client : vStClients)
 		{
-			if (line == lineToUpdate)
-			{
-				line = lineToUpdateTo;
-			}
-			myFile << line << "\n";
+			myFile << convertStAccountToStringLine(client) << "\n";
 		}
 		myFile.close();
 	}
 }
 
-void askForDeletAccount(vector <string> vStringClients, string AccountNumber,  stClient client, string filName)
+void updateAccount(string AccountNumber, vector <stClient>& vStClients, string fileName)
 {
-	char confirmUpdate;
-	string accountToUpdate;
-	string updatedAccount;
-	stClient stUpdatedClient;
+	char confirmDelete;
+	stClient client;
 
-	if (findAccountWithAccountNumber(vStringClients, AccountNumber, client))
+	if (findAccountWithAccountNumber(vStClients, AccountNumber, client))
 	{
 		printStClientData(client);
-		confirmUpdate = readAnswerToDeleteAccount();
 
-		if (confirmUpdate == 'y' || confirmUpdate == 'Y')
+		confirmDelete = readAnswerToUpdateAccount();
+
+		if (confirmDelete == 'y' || confirmDelete == 'Y')
 		{
-			accountToUpdate = convertStAccountToStringLine(client);
-			stUpdatedClient = readClient();
-			updatedAccount = convertStAccountToStringLine(stUpdatedClient);
-
-
-			updateAccountWithAccountNumber(filName, vStringClients, accountToUpdate, updatedAccount);
-			cout << "Client deleted Successfully!\n";
+			client = updateClient(vStClients, client, AccountNumber);
+			saveAccountsToFile(fileName, vStClients);
+			cout << "\n\nClient deleted Successfully!\n";
 		}
-		else
-			return;
 	}
 	else
-		cout << "\n Client with account Number (" << AccountNumber << ") Not Found!\n";
+	{
+		cout << "\n\n Client with account Number (" << AccountNumber << ") Not Found!\n";
+	}
+
+
 }
 
 int main()
 {
 	string fileName = "clientInfo.txt";
-	stClient client;
-	vector <string> vStringClients = vectorStringtAccounts(fileName);
+	vector <stClient> vStClients = loadDataToVector(fileName);
 	string AccountNumber = readAccountNumber();
 
-	askForDeletAccount(vStringClients, AccountNumber, client, fileName);
 
+	updateAccount(AccountNumber, vStClients, fileName);
 
-
-
-
+	for (stClient& client : vStClients)
+		printStClientData(client);
 
 	system("pause>0");
 	return 0;
 }
+
+
+
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <fstream>
+//#include <iomanip>
+//using namespace std;
+//
+//
+//struct stClient
+//{
+//	string Number;
+//	string pinCode;
+//	string name;
+//	string phone;
+//	double balance;
+//};
+//
+//stClient readClient()
+//{
+//	stClient client;
+//
+//	cout << "Please Enter Client Data?\n\n";
+//
+//	cout << "Enter Account Number? ";
+//	getline(cin >> ws, client.Number);
+//
+//	cout << "Enter Name? ";
+//	getline(cin, client.name);
+//
+//
+//	cout << "Enter PinCode? ";
+//	cin >> client.pinCode;
+//
+//	cout << "Enter Phone ";
+//	cin >> client.phone;
+//
+//	cout << "Enter Account balance ";
+//	cin >> client.balance;
+//
+//	return client;
+//}
+//
+//string readAccountNumber()
+//{
+//	string AccountNumber = "";
+//	cout << "\n Please Enter Account Number? ";
+//	cin >> AccountNumber;
+//
+//	return AccountNumber;
+//}
+//
+//char readAnswerToDeleteAccount()
+//{
+//	char confirmUpdate;
+//
+//	cout << "Are You sure to update This Account? y/n? ";
+//	cin >> confirmUpdate;
+//
+//	return confirmUpdate;
+//
+//}
+//
+//vector <string> LineDataToVector(string line)
+//{//A150#//#1234#//#Gomaa ElSherbini#//#00925365232#//#5000.000000
+//
+//	vector <string> vClient;
+//	string delim = "#//#";
+//	short pos = 0;
+//	string clientItem = "";
+//
+//	while ((pos = line.find(delim)) != -1)
+//	{
+//		clientItem = line.substr(0, pos);
+//		if (clientItem != "")
+//		{
+//			vClient.push_back(clientItem);
+//		}
+//		line.erase(0, pos + delim.length());
+//	}
+//	if (line != "")
+//	{
+//		vClient.push_back(line);
+//	}
+//	return vClient;
+//}
+//
+//stClient VectorDataToStClient(vector <string> vClient)
+//{
+//	stClient client;
+//
+//	client.Number = vClient[0];
+//	client.pinCode = vClient[1];
+//	client.name = vClient[2];
+//	client.phone = vClient[3];
+//	client.balance = stod(vClient[4]);
+//
+//	return client;
+//}
+//
+//vector <string> vectorStringtAccounts(string fileName)
+//{
+//	fstream Clients;
+//	string line;
+//	vector <string> vStringClients;
+//
+//	Clients.open(fileName, ios::in);
+//
+//	if (Clients.is_open())
+//	{
+//		while (getline(Clients, line))
+//		{
+//			if (line != "")
+//			{
+//				vStringClients.push_back(line);
+//			}
+//		}
+//		Clients.close();
+//	}
+//	return vStringClients;
+//}
+//
+//string convertStAccountToStringLine(stClient clientRecord)
+//{
+//	string recordAataLine;
+//	string delim = "#//#";
+//
+//	recordAataLine = clientRecord.Number;
+//	recordAataLine += delim + clientRecord.pinCode;
+//	recordAataLine += delim + clientRecord.name;
+//	recordAataLine += delim + clientRecord.phone;
+//	recordAataLine += delim + to_string(clientRecord.balance);
+//
+//	return recordAataLine;
+//
+//}
+//
+//void printStClientData(stClient client)
+//{
+//	cout << "\nThe following are the eclient details:\n\n";
+//
+//	cout << " Account Number:  " << client.Number << endl;
+//	cout << " Pin Code:  " << client.pinCode << endl;
+//	cout << " Name:  " << client.name << endl;
+//	cout << " Phone: " << client.phone << endl;
+//	cout << " Balance: " << client.balance << endl;
+//
+//}
+//
+//bool findAccountWithAccountNumber(vector <string> vStringClients, string AccountNumber, stClient& client)
+//{
+//	vector <string> vClient;
+//	stClient sClient;;
+//
+//	for (string line : vStringClients)
+//	{
+//		sClient = VectorDataToStClient(LineDataToVector(line));
+//		if (sClient.Number == AccountNumber)
+//		{
+//			client = sClient;
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
+//
+//void updateAccountWithAccountNumber(string fileName, vector <string> vFileContent, string lineToUpdate, string lineToUpdateTo)
+//{
+//	fstream myFile;
+//	myFile.open(fileName, ios::out);
+//
+//	if (myFile.is_open())
+//	{
+//		for (string& line : vFileContent)
+//		{
+//			if (line == lineToUpdate)
+//			{
+//				line = lineToUpdateTo;
+//			}
+//			myFile << line << "\n";
+//		}
+//		myFile.close();
+//	}
+//}
+//
+//void askForDeletAccount(vector <string> vStringClients, string AccountNumber,  stClient client, string filName)
+//{
+//	char confirmUpdate;
+//	string accountToUpdate;
+//	string updatedAccount;
+//	stClient stUpdatedClient;
+//
+//	if (findAccountWithAccountNumber(vStringClients, AccountNumber, client))
+//	{
+//		printStClientData(client);
+//		confirmUpdate = readAnswerToDeleteAccount();
+//
+//		if (confirmUpdate == 'y' || confirmUpdate == 'Y')
+//		{
+//			accountToUpdate = convertStAccountToStringLine(client);
+//			stUpdatedClient = readClient();
+//			updatedAccount = convertStAccountToStringLine(stUpdatedClient);
+//
+//
+//			updateAccountWithAccountNumber(filName, vStringClients, accountToUpdate, updatedAccount);
+//			cout << "Client deleted Successfully!\n";
+//		}
+//		else
+//			return;
+//	}
+//	else
+//		cout << "\n Client with account Number (" << AccountNumber << ") Not Found!\n";
+//}
+//
+//int main()
+//{
+//	string fileName = "clientInfo.txt";
+//	stClient client;
+//	vector <string> vStringClients = vectorStringtAccounts(fileName);
+//	string AccountNumber = readAccountNumber();
+//
+//	askForDeletAccount(vStringClients, AccountNumber, client, fileName);
+//
+//
+//
+//
+//
+//
+//	system("pause>0");
+//	return 0;
+//}
